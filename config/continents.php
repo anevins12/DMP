6b855eb233d921d5f4ba4485a0f2b32488be9d13
andrew nevins <andrew@red-robot.co.uk>
2012-09-02 19:12:15 +0100

    tracked files

---
+++ config/continents.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Defining continents' geographical locations */
+/*
+ *							   LATITUDE                               LONGITUDE
+*/
+
+define('LAT_LONG_SEPARATOR', ',');
+
+define('AFRICA'         ,   '46.0730555556' . LAT_LONG_SEPARATOR . '-100.546666667' );
+define('ANTARCTICA'     ,   '-83.3594444444' . LAT_LONG_SEPARATOR . '16.5233333333' );
+define('ASIA'           ,   '29.8405555556' . LAT_LONG_SEPARATOR . '89.2966666667' );
+define('EUROPE'         ,   '48.6908333333' . LAT_LONG_SEPARATOR . '9.14055555556' );
+define('NORTH_AMERICA'  ,   '7.18805555556' . LAT_LONG_SEPARATOR . '21.0936111111' );
+define('SOUTH_AMERICA'  ,   '-14.6047222222' . LAT_LONG_SEPARATOR . '-57.6561111111' );
+
+?>
---
+++ controller/locations.php
@@ -0,0 +1,45 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of locations
+ *
+ * @author andrew
+ */
+class Locations {
+
+	function __construct() {
+
+		require_once( '../geocoder/src/autoload.php' );
+
+	}
+
+	function geocoder() {
+	 
+		// Create an adapter
+		$adapter  = new \Geocoder\HttpAdapter\BuzzHttpAdapter();
+
+		// Create a Geocoder object and pass it your adapter
+		$geocoder = new \Geocoder\Geocoder();
+
+		// Then, register all providers your want
+		$geocoder->registerProviders(array(
+
+			new \Geocoder\Provider\YahooProvider(
+				$adapter, '<YAHOO_API_KEY>', $locale
+			),
+			new \Geocoder\Provider\IpInfoDbProvider(
+				$adapter, '<IPINFODB_API_KEY>'
+			),
+			new \Geocoder\Provider\HostIpProvider($adapter),
+
+			// your provider here
+		));
+
+		var_dump($geocoder->geocode('68.145.37.34'));exit;
+	}
+}
+?>
