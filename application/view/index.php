9e769a8c549cf978af4a376dec0a793262f601a9
andrew nevins <andrew@red-robot.co.uk>
2012-11-16 23:36:15 +0000

    home page header, breadcrumbs completed

--- application/view/index.php
+++ application/view/index.php
@@ -5,7 +5,7 @@
 		<link type="text/css" rel="stylesheet" href="/application/assets/css/resets.css" />
 		<link type="text/css" rel="stylesheet" href="/application/assets/css/fonts.css" />
 		<link type="text/css" rel="stylesheet" href="/application/assets/css/960_12_col.css" />
-		<link type="text/css" rel="stylesheet" href="/application/assets/css/global-nav.css" />
+		<link type="text/css" rel="stylesheet" href="/application/assets/css/nav.css" />
 		<link type="text/css" rel="stylesheet" href="/application/assets/css/style.css" />
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 		<script type="text/javascript" src="/application/assets/js/modernizr-2.5.3.min.js"></script>
@@ -14,18 +14,35 @@
 	<body>
 		<div class="container_12">
 			<header>
-				<div class="logo grid_6">
+				<hgroup class="logo grid_6">
 					<h1><a href="/application/">How Happy</a></h1>
 					<h2>Measuring Happiness un UK Cities from Twitter</h2>
-				</div>
+				</hgroup>
 				<nav class="grid_6">
 					<ul>
-						<li class="grid_2 alpha"><a href="#"><span class="home"></span><span class="txt">Home</span></a></li>
+						<li class="grid_2 alpha"><a href="#" class="selected"><span class="home"></span><span class="txt">Home</span></a></li>
 						<li class="grid_2"><a href="#"><span class="cities"></span><span class="txt">Happiest Cities</span></a></li>
 						<li class="grid_2 omega"><a href="#"><span class="graphs"></span><span class="txt">Mood Graphs</span></a></li>
 					</ul>
 				</nav>
 			</header>
+		</div>
+		<div class="container_12" id="main">
+			<nav class="grid_12 alpha" id="breadcrumbs">
+				<ul>
+					<li><span class="home icon"></span><span class="txt">Home</span></li>
+				</ul>
+			</nav>
+			<div class="grid_3 alpha">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Vivamus cursus ultrices urna, vitae consequat massa suscipit eget.
+					Aliquam erat volutpat. In hac habitasse platea dictumst.
+					Aliquam erat volutpat. Duis id erat mi. Ut nec dui leo,
+					eu molestie ante. Mauris risus odio, vestibulum at convallis
+					vitae, vehicula eu.
+				</p>
+			</div>
 		</div>
 	</body>
 </html>
