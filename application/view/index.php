f82b2bae93ba28929a313455411edef1e84cc959
andrew nevins <andrew@red-robot.co.uk>
2012-11-17 17:45:43 +0000

    home page (view) done

--- application/view/index.php
+++ application/view/index.php
@@ -12,36 +12,61 @@
 	</head>
 
 	<body>
-		<div class="container_12">
-			<header>
-				<hgroup class="logo grid_6">
-					<h1><a href="/application/">How Happy</a></h1>
-					<h2>Measuring Happiness un UK Cities from Twitter</h2>
-				</hgroup>
-				<nav class="grid_6">
+		<div id="wrapper">
+			<div class="container_12">
+				<header>
+					<hgroup class="logo grid_6">
+						<h1><a href="/application/">How Happy</a></h1>
+						<h2>Measuring Happiness un UK Cities from Twitter</h2>
+					</hgroup>
+					<nav class="grid_6">
+						<ul>
+							<li class="grid_2 alpha"><a href="#" class="selected"><span class="home"></span><span class="txt">Home</span></a></li>
+							<li class="grid_2"><a href="#"><span class="cities"></span><span class="txt">Happiest Cities</span></a></li>
+							<li class="grid_2 omega"><a href="#"><span class="graphs"></span><span class="txt">Mood Graphs</span></a></li>
+						</ul>
+					</nav>
+				</header>
+			</div>
+			<div class="container_12" id="main">
+				<nav class="grid_12 alpha" id="breadcrumbs">
 					<ul>
-						<li class="grid_2 alpha"><a href="#" class="selected"><span class="home"></span><span class="txt">Home</span></a></li>
-						<li class="grid_2"><a href="#"><span class="cities"></span><span class="txt">Happiest Cities</span></a></li>
-						<li class="grid_2 omega"><a href="#"><span class="graphs"></span><span class="txt">Mood Graphs</span></a></li>
+						<li><span class="home icon"></span><span class="txt">Home</span></li>
 					</ul>
 				</nav>
-			</header>
-		</div>
-		<div class="container_12" id="main">
-			<nav class="grid_12 alpha" id="breadcrumbs">
-				<ul>
-					<li><span class="home icon"></span><span class="txt">Home</span></li>
-				</ul>
-			</nav>
-			<div class="grid_3 alpha">
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Vivamus cursus ultrices urna, vitae consequat massa suscipit eget.
-					Aliquam erat volutpat. In hac habitasse platea dictumst.
-					Aliquam erat volutpat. Duis id erat mi. Ut nec dui leo,
-					eu molestie ante. Mauris risus odio, vestibulum at convallis
-					vitae, vehicula eu.
-				</p>
+				<div class="grid_3 alpha description">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Vivamus cursus ultrices urna, vitae consequat massa suscipit eget.
+						Aliquam erat volutpat.
+					</p>
+				</div>
+				<div class="grid_3">
+					<h3><a href="#">Happiest Cities<span class="pointer"></span></a></h3>
+					<div class="image">
+						<a href="#">
+							<img src="/application/assets/i/home-icon-1.png" alt="Happiest Cities" />
+						</a>
+					</div>
+				</div>
+				<div class="grid_3">
+					<h3><a href="#">Mood Graphs<span class="pointer"></span></a></h3>
+					<div class="image">
+						<a href="#">
+							<img src="/application/assets/i/home-icon-2.png" alt="Mood Graphs" />
+						</a>
+					</div>
+				</div>
+				<div class="grid_3 omega description">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Vivamus cursus ultrices urna, vitae consequat massa suscipit eget.
+						Aliquam erat volutpat.
+					</p>
+				</div>
+			</div>
+			<div class="container_12" id="footer">
+				<!-- nothing to go in here -->
 			</div>
 		</div>
 	</body>
