c2cd487a3cb3530e1548ec02ceb67771f2970757
andrew nevins <andrew@red-robot.co.uk>
2013-04-01 14:26:20 +0100

    users controller commented

--- application/controller/users.php
+++ application/controller/users.php
@@ -1,16 +1,23 @@
 <?php
-/**
- * Description of tweets
- *
- * @author andrew
- */
 
+/**
+ * Grabs data from the Usersmodel and returns it
+ *
+ *
+ * @author_name Andrew Nevins
+ * @author_no 09019549
+ */
 class Users {
 
     function __construct() {
 		include_once ( dirname(__FILE__) . '/../model/usersmodel.php' );
 	}
 
+	/**
+	 * Gets all users (remember, it gets unhappy users) and convert it to a JSONstring
+	 *
+	 * @return  JSONstring
+	 */
 	function getAllUsers() {
 		$users = new Usersmodel();
 		$allUsers = $users->getAllUsers();
